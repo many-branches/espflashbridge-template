c343f05f6b75ca2db307e55037638bd834329ac4
Liam Monninger <l-monninger@Liams-MacBook-Air.local>
2023-06-15 13:20:48 -0700

    adding working directory controls.

--- .flashbridge/flash.sh
+++ .flashbridge/flash.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [[ "$FLASH_MODE" == "ssh" ]]; then
-ssh -t $HOST_USER@$HOST_HOSTNAME "/bin/bash -c 'espflash flash --monitor $1'"
+ssh -t $HOST_USER@$HOST_HOSTNAME "/bin/bash -c 'cd $HOST_WD && espflash flash --monitor $1'"
 elif [[ "$FLASH_MODE" == "server" ]]; then
 ./.flashbridge/client.sh flash --monitor $1 --port $FLASH_PORT
 else
